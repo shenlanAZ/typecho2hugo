ce065d452d6bb10cefd3529af5cae54739755328
shenlanAZ <shenlanluck@gmail.com>
2021-10-16 13:18:22 +0800

    Update export2hugo.js

--- export2hugo.js
+++ export2hugo.js
@@ -28,6 +28,31 @@ const metas = db.prepare(`SELECT mid,name,type FROM ${prefix}metas`).all().reduc
   };
   return obj;
 }, {});
+
+Date.prototype.format = function(format){
+  var o = {
+      "M+" : this.getMonth()+1, //month
+      "d+" : this.getDate(), //day
+      "h+" : this.getHours(), //hour
+      "m+" : this.getMinutes(), //minute
+      "s+" : this.getSeconds(), //second
+      "q+" : Math.floor((this.getMonth()+3)/3), //quarter
+      "S" : this.getMilliseconds() //millisecond
+  }
+
+  if(/(y+)/i.test(format)) {
+      format = format.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+  }
+
+  for(var k in o) {
+      if(new RegExp("("+ k +")").test(format)) {
+          format = format.replace(RegExp.$1, RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));
+      }
+  }
+  return format;
+}
+
+
 for (const post of stmt.iterate()) {
   const { cid, title, slug, created, text, type } = post;
   if (type !== 'post' || !text) {
@@ -55,8 +80,7 @@ for (const post of stmt.iterate()) {
   const postMD =
 `---
 title: "${title}"
-slug: "${slug}"
-date: ${(new Date(created * 1000)).toISOString()}
+date: ${(new Date(created * 1000)).format("yyyy-MM-dd hh:mm:ss")}
 categories:
 ${categories.join('\n')}
 tags:
